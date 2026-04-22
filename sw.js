098e783c5a97ba30717616e6e649c667f6ad786f
nesmanagahhassan-cmd <nesmanagahhassan@gmail.com>
2026-04-22 11:51:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,45 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('car-store-v1').then((cache) => cache.addAll([
-      '/',
-      '/index.html'
-    ]))
-  );
+const cacheName = 'car-hub-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
